f4d1f199508178ff20d4220eb17c8185f9f1d612
LEEHJ <LEEHJ@LEEHJ>
2017-09-20 14:35:51 +0900

    1

--- AppTest/src/math.java
+++ AppTest/src/math.java
@@ -6,7 +6,7 @@ public class math {
 	public static void main(String[] args) {
 
 		
-		String test = "2017.08.02 17:45";  
+		String test = "2017.08.02 17:45"; 
 		
 		System.out.println(test.replaceAll("\\.", "").replaceAll("\\:", "").replaceAll(" ", ""));
 	
